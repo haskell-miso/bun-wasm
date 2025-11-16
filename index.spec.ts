f515f35c74f21a1ad269f27899fe0cc4ecf1305e
dmjio <code@dmj.io>
2025-11-15 21:56:53 -0600

    Add DOM operations tests.
    - [x] Add test for DOM operations through WASM (`domAppend`)
    - [x] Factor out instace exports into app.ts
    - [x] Delete `static/index.js`

--- index.spec.ts
+++ index.spec.ts
@@ -1,29 +1,6 @@
 /* imports */
 import { test, expect, describe, afterEach, beforeAll } from 'bun:test';
-import ghc_wasm_jsffi from "./public/ghc_wasm_jsffi.js";
-import { WASI, OpenFile, File, ConsoleStdout } from "@bjorn3/browser_wasi_shim";
-
-/* options */
-const args = [];
-const env = ["GHCRTS=-H64m"];
-const fds = [
-  new OpenFile(new File([])), // stdin
-  ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ''${msg}`)),
-  ConsoleStdout.lineBuffered((msg) => console.warn(`[WASI stderr] ''${msg}`)),
-];
-const options = { debug: false };
-const wasi = new WASI(args, env, fds, options);
-
-const instance_exports = {};
-
-const wasmBuffer = await Bun.file("./public/app.wasm").arrayBuffer();
-const { instance } = await WebAssembly.instantiate(wasmBuffer, {
-  wasi_snapshot_preview1: wasi.wasiImport,
-  ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
-});
-
-Object.assign(instance_exports, instance.exports);
-wasi.initialize(instance);
+import { instance } from './app.ts';
 
 /* silence */
 beforeAll(() => {
@@ -38,7 +15,7 @@ afterEach(() => {
   document.body.innerHTML = '';
 });
 
-const { add, sub } = await instance.exports;
+const { add, sub, domAppend } = await instance.exports;
 
 /* tests */
 describe('Should test arithmetic', () => {
@@ -50,6 +27,15 @@ describe('Should test arithmetic', () => {
   test('2 - 8 should equal -6', () => {
       sub(2,8).then ((result) => {
         expect(result).toEqual(-6);
+      });
+  });
+});
+
+describe('Should perform DOM operations', () => {
+  test('DOM append test', () => {
+      expect(document.body.childNodes.length).toEqual(0);
+      domAppend().then (() => {
+        expect(document.body.childNodes.length).toEqual(1);
       });
   });
 });
